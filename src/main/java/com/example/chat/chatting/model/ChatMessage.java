0293586c0dc008233fd66349e86a9c039f113c6f
seonghan.i <shim@gcsc.co.kr>
2024-06-19 13:34:35 +0900

    message db save 추가

--- src/main/java/com/example/chat/chatting/model/ChatMessage.java
+++ src/main/java/com/example/chat/chatting/model/ChatMessage.java
@@ -26,8 +26,6 @@ public class ChatMessage extends BaseEntity {
 
     private String message;
 
-    private LocalDateTime messageTime;
-
     private static void saveChatMessage(){
 
     }
--- src/main/java/com/example/chat/chatting/model/ChatMessageDto.java
+++ src/main/java/com/example/chat/chatting/model/ChatMessageDto.java
@@ -1,13 +1,21 @@
 package com.example.chat.chatting.model;
 
+import com.example.chat.chatting.repository.ChatMessageRepository;
+import com.example.chat.chatting.repository.ChatRoomRepository;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 public class ChatMessageDto {
-
+    public ChatRoomRepository chatRoomRepository;
+    public ChatMessageDto(ChatRoomRepository chatRoomRepository){
+        this.chatRoomRepository = chatRoomRepository;
+    }
     private MessageType messageType;
 
     private String chatRoomId;
@@ -15,5 +23,14 @@ public class ChatMessageDto {
     private String sender;
 
     private String message;
+
+    public ChatMessage toEntity(){
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setMessageType(messageType);
+        chatMessage.setSender(sender);
+        chatMessage.setMessage(message);
+        chatMessage.setChatRoom(chatRoomRepository.findByRoomId(chatRoomId));
+        return chatMessage;
+    }
 
 }
